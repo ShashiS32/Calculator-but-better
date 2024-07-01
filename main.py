35dcab1db050cfd1e332bc6f907838f1a37098c4
Shashwat Singh <shashwats17@gmail.com>
2024-07-01 10:54:50 -0400

    Calculator but better

---
+++ main.py
@@ -0,0 +1,55 @@
+import math
+
+operations = ["Addition", "Subtraction", "Multiplication", "Division"]
+
+
+while True:
+    choice = (input("What operation do you want to choose? (Addition, Subtraction, Multiplication, Division) ")).capitalize()
+
+    if choice in operations:
+        break
+    else:
+        print("Invalid operation choice")
+        continue
+        
+
+number = int(input("How many numbers are you operating with? " ))
+
+
+nums_they_working_with = []
+
+for nums in range (number):
+    x = float(input(f"Enter number {nums + 1} "))
+    nums_they_working_with.append(x)
+
+
+def addition():
+    result = sum(nums_they_working_with)
+    print(result)
+
+
+def multiplication():
+    result = math.prod(nums_they_working_with)
+    print(result)
+
+def division():
+    result = math.remainder(nums_they_working_with)
+    print(result)
+
+
+def subtraction():
+    result = nums_they_working_with[0]
+    for num in nums_they_working_with[1:]:
+        result -= num
+    print(result)
+
+
+
+if choice == "Addition":
+    addition()
+elif choice == "Subtraction":
+    subtraction()
+elif choice.capitalize() == "Multiplication":
+    multiplication()
+elif choice.capitalize() == "Division":
+    division()
